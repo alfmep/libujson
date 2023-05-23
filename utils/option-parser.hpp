88032d52d4f1a629e13e207e069b131b0df58e4b
Dan <dan@ultramarin.se>
2023-05-23 14:00:09 +0200

    Corrected faulty documentation in a comment and added missing copyright info.

--- utils/option-parser.hpp
+++ utils/option-parser.hpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2023 Dan Arrhenius <dan@ultramarin.se>
+ *
+ * This file is part of ujson.
+ *
+ * ujson is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef OPTION_PARSER_HPP
 #define OPTION_PARSER_HPP
 
@@ -13,7 +31,7 @@ struct opt_t {
         optional, /**< The option has an optional argument. */
         required  /**< The option requires an argument. */
     } arg;
-    int id;       /**< An option id. If -1, the value of 'short_opt' is used. */
+    int id;       /**< An option id. If 0, the value of 'short_opt' is used. */
 };
 using optlist_t = std::vector<opt_t>;
 
